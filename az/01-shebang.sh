ff661be1534860fd9a55e7789c119bf88a1d53a3
lakshmi bhavani <lakshmi.bhavani888@gmail.com>
2023-06-23 23:12:50 +0530

    Auto push

---
+++ az/01-shebang.sh
@@ -0,0 +1,23 @@
+#!1/bin/bash
+
+## start of the line in any bash script should always be a SHEBANG NOTATION
+
+## Apart from the first line, none of the lines are considered as shebang
+
+## shebang notation always start with #
+
+## using the SHEBANG Notation,we are telling the system the interpretor to use to run this script.
+
+## your server would have java,go,python,nodejs,shell installed. (interpretor)
+
+## Now if you run the script,how system will come to know which interpretor it has to use,that's where SHEBANG Notation helps
+
+
+#### how to run a linux bash script ?
+
+. bash scriptName.sh
+. sh scriptName.sh
+. ./ scriptName
+
+
+type java : /bin/java
