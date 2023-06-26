bf6ecf4b72bceecb4e720b1bff4752cbc8e6b6c1
lakshmi bhavani <lakshmi.bhavani888@gmail.com>
2023-06-26 20:16:44 +0530

    Auto push

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,4 +2,22 @@
 # variables is used to store pass values
 # a=10
 # name=bhavani
+# batch=54
+#topic=shell scripting
+#This is how to declare variables in bash
+#a=10
+#b=20
+#c=30 
 
+#syntax :$variable name : $ is going to print the values of the variable
+echo$a
+echo value of variable the variable a is $a
+echo value of variable the variable a is ${a}
+echo "value of the variable a is ${a}"
+echo "value of the variable d is ${d}"
+# if you try to print a variable that is not declared,bash will consider that as a null rather an error
+rm -rf /data/${Folder_To_DELETE} -> rm -rf /data
+                             
+
+# "Double Quotes "
+# 'single  Quotes'
